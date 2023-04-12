63c991d5beb45a1144dc37372d2ae55f382d4770
samuelcostab <samuelufcbrito@gmail.com>
2023-04-12 09:23:24 -0300

    fix: adjusting broken test

--- src/functions/__tests__/LajeCalculateMetricsTest.spec.js
+++ src/functions/__tests__/LajeCalculateMetricsTest.spec.js
@@ -2,10 +2,10 @@ import { calcularLajotas, calcularTrilhos } from "../LajeCalculateMetrics";
 
 describe("Calculate Metrics tests", () => {
   test("should calculate lajotas correctly", () => {
-    expect(Number(calcularLajotas(2.5,2).toFixed(2))).toEqual(60.00);
+    expect(Number(calcularLajotas(2.5,2).toFixed(2))).toEqual(60);
   })
   
   test("should calculate trilhos correctly", () => {
-    expect(Number(calcularTrilhos(2.5,2).toFixed(2))).toEqual(5.95);
+    expect(Number(calcularTrilhos(2.5,2).toFixed(2))).toEqual(6);
   })
 });
